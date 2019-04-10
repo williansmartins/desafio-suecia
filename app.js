533aae376e12721b43ca73e99906f3489f608980
Willians Martins de Morais <wsms@gft.com>
2019-04-10 20:14:21 -0300

    refatoring code

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ module.exports = {
 		let size = transactions.length;
 
 		if(size>0){
-			for (var i = size; i > 0; i--) {
+			for (let i = size; i > 0; i--) {
 
-				let transaction = transactions[i-1];
-				let categoryOk = (transaction.category == category );
-				let periodOk   = (new Date(transaction.time.substring(0,10)) >= start && new Date(transaction.time.substring(0,10)) < end );
+				let transaction      = transactions[i-1];
+				let categoryOk       = (transaction.category == category );
+				let transaction_time = new Date(transaction.time.substring(0,10));
+				let periodOk         = (transaction_time >= start) && (transaction_time < end) ;
 
 				if(categoryOk && periodOk){
 					total += transaction.amount;
